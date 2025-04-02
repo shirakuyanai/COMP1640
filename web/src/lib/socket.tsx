57b8ac8cbad1236d3baa3255ba3b4070885accab
satan <keebin01@gmail.com>
2025-04-03 00:37:59 +0700

    5

--- web/src/lib/socket.tsx
+++ web/src/lib/socket.tsx
@@ -4,7 +4,7 @@ let socket: any
 
 export const initializeSocket = (username: string) => {
 	if (!socket) {
-		socket = io('http://localhost:5000', {
+		socket = io('import.meta.env.VITE_API_BASE_URL', {
 			transports: ['websocket'],
 			auth: { username },
 		})
